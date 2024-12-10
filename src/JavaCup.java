0614905223a6cfac8280d50050658e9fd791e3ff
Hasti <hasti@smartech.ir>
2024-12-10 21:47:30 +0330

    Perf: enhance performance of temp file

--- src/JavaCup.java
+++ src/JavaCup.java
@@ -2,8 +2,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JavaCup {
-    public static void main(String[] args)
-    {
+    static int max_i = 10000;
+    static int max_j = 2000;
+
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Press number1: ");
         int i = scanner.nextInt();
@@ -14,20 +17,20 @@ public class JavaCup {
         temp();
         eval(i, j, k);
     }
-    public static void eval(int i, int j, int k)
-    {
-        if (i * i + j * j == k * k || i * i == j * j + k * k || j * j == i * i + k * k)
-        {
+
+    public static void eval(int i, int j, int k) {
+        if (i * i + j * j == k * k || i * i == j * j + k * k || j * j == i * i + k * k) {
             System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
-        else { System.out.println("NO"); }
     }
+
     public static void temp() {
-        ArrayList a = new ArrayList();
-        for (int i = 0; i < 10000; i++)
-        {
-            for (int j = 0; j < 20000; j++) {
-                a.add(i + j);
+        int [][] a = new int[max_i][max_j];
+        for (int i = 0; i < max_i; i++) {
+            for (int j = 0; j < max_j; j++) {
+                a[i][j] =  i + j;
             }
         }
     }
